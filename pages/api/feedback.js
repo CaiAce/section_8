d72fb812101de3a51538cf6d2cda3051a6f713b4
Cailean <chorton44@aceproject.space>
2023-02-03 15:53:28 -0600

    Added functionality
    Added the capability to send GET and POST requests to the API route.

---
+++ pages/api/feedback.js
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import path from 'path';
+
+function buildFeedbackPath() {
+
+    return path.join(process.cwd(), 'data', 'feedback.json');
+}
+
+function extractFeedback(filePath) {
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+
+    return data;
+}
+
+function handler(req, res) {
+  if (req.method === "POST") {
+    const incomingEmail = req.body.email;
+    const incomingFeedback = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: incomingEmail,
+      text: incomingFeedback,
+    };
+
+    // Store to a database/file
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+
+    res.status(201).json({message: "Success", feedback: newFeedback});
+  }
+  else{
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+
+    res.status(200).json({feedback: data});
+  }
+  
+}
+
+export default handler;
